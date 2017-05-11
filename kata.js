f181c0f7b63ca31743bb9b66f3fa49207657c48c
rendaniluk <lukhwa11582924>
2017-05-11 11:03:59 +0200

    kata modification

--- kata.js
+++ kata.js
@@ -1,18 +1,43 @@
-//split string by commas
 module.exports = {
-    firstString: function(allDeals) {
+    //split string by commas
+    separateStringComma: function(allDeals) {
         var newString = allDeals.split(',');
         console.log(newString);
+        return newString;
+    },
+    //split string by for
+    separateStringFor: function(separateStringComma) {
         var actualArr = [];
-
-        for (var i = 0; i < newString.length; i++) {
-            var myString = newString[i].split('for');
+        for (var i = 0; i < separateStringComma.length; i++) {
+            var myString = separateStringComma[i].split('for');
+            //array of object
             actualArr.push({
                 Qty: myString[0],
-                Price: ((Number(myString[1].replace('R', ''))) / (Number(myString[0]))).toFixed(2)
+                Price: (Number(myString[1].replace('R', ''))).toFixed(2)
             });
         }
         console.log(actualArr);
-        return newString;
-    }
+        return actualArr;
+    },
+    calculateDealPerAvo: function(separateStringFor) {
+        var deals = [];
+        for (var i = 0; i < separateStringFor.length; i++) {
+            var pricePerAvo = separateStringFor[i];
+            deals.push((pricePerAvo.Price / pricePerAvo.Qty).toFixed(2));
+        }
+        console.log(deals);
+        return deals;
+    },
+  findCheapestDeal: function(calculateDealPerAvo) {
+    var cheapestDeal = Math.min.apply(null, calculateDealPerAvo).toFixed(2);
+    console.log(cheapestDeal);
+    return cheapestDeal;
+  },
+    findExpensiveDeal: function(calculateDealPerAvo) {
+        var expensiveDeal = Math.max.apply(null, calculateDealPerAvo).toFixed(2);
+        console.log(expensiveDeal);
+        return expensiveDeal;
+      }
+
+
 };
--- test/kata.test.js
+++ test/kata.test.js
@@ -4,25 +4,66 @@ describe('The kata function', function() {
     it(
         'It should return the array of strings separated by commas',
         function() {
-            assert.deepEqual([ '1 for R3', ' 2 for R7', ' 3 for R10', ' 5 for R14.50' ], kata.firstString("1 for R3, 2 for R7, 3 for R10, 5 for R14.50"));
+            assert.deepEqual(['1 for R3', ' 2 for R7', ' 3 for R10', ' 5 for R14.50'], kata.separateStringComma("1 for R3, 2 for R7, 3 for R10, 5 for R14.50"));
         });
-        // it(
-        //     'It should return the array of strings separated by commas',
-        //     function() {
-        //         assert.deepEqual([ '1 for R3', ' 2 for R7', ' 3 for R10', ' 5 for R14.50' ], kata.secondString("1 for R3, 2 for R7, 3 for R10, 5 for R14.50"));
-        //     });
+    it(
+        'It should return the array of object separated by for',
+        function() {
+            assert.deepEqual([{
+                    Qty: '1 ',
+                    Price: '3.00'
+                },
+                {
+                    Qty: ' 2 ',
+                    Price: '7.00'
+                },
+                {
+                    Qty: ' 3 ',
+                    Price: '10.00'
+                },
+                {
+                    Qty: ' 5 ',
+                    Price: '14.50'
+                }
+            ], kata.separateStringFor(['1 for R3', ' 2 for R7', ' 3 for R10', ' 5 for R14.50']));
+        });
+    it('It should return array of Price per avo', function() {
+        assert.deepEqual(['3.00', '3.50', '3.33', '2.90'], kata.calculateDealPerAvo([{
+                Qty: '1 ',
+                Price: '3.00'
+            },
+            {
+                Qty: ' 2 ',
+                Price: '7.00'
+            },
+            {
+                Qty: ' 3 ',
+                Price: '10.00'
+            },
+            {
+                Qty: ' 5 ',
+                Price: '14.50'
+            }
+        ]))
+    });
+    it('It should return cheapest deal of avo', function() {
+        assert.deepEqual('2.90', kata.findCheapestDeal(['3.00', '3.50', '2.90', '3.33']))
+    });
+    it('It should return expensive deal of avo', function() {
+        assert.deepEqual('3.50', kata.findExpensiveDeal(['3.00', '3.50', '2.90', '3.33']))
+    });
 });
 
 
-getPrice(deal);
-
-var deals = getDeals('');
-
-function dealsWithPrice(deals){
-
-  // over deals
-  getPrice(deal)
-
-}
-
-var dealWithPrice = dealsWithPrice(deals);
+// getPrice(deal);
+//
+// var deals = getDeals('');
+//
+// function dealsWithPrice(deals){
+//
+//   // over deals
+//   getPrice(deal)
+//
+// }
+//
+// var dealWithPrice = dealsWithPrice(deals);
